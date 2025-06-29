0ea44a9b04558fb5718d4d5ddb5b094edd3546c7
wahyuanandaa <wahyuanandaa@gmail.com>
2025-06-29 09:23:47 +0700

    update 3

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,14 @@ import {
   Footer
 } from "./components/sections"
 import UrlShortener from "./components/UrlShortener.jsx"
+import ConnectionStatus from "./components/ConnectionStatus.jsx"
+import ApiStatus from "./components/ApiStatus.jsx"
 
 const App = () => {
   return (
     <main className="bg-neutral-grayTwo">
+      <ConnectionStatus />
+      <ApiStatus />
       <div className="bg-white pb-36">
         <Header />
         <Hero />
